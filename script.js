61b3b308dc05256477a026c7a598240194f8712d
Gaëtan Tokotuu <gaetan.tokotuu@gmail.com>
2020-12-04 16:07:02 +0100

    Rewrite DFS function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ class Graph {
     constructor() {
         this.nComponent = 0;
         this.adjList = new Map();
+        this.cycleList = new Map();
         this.visited = new Set();
     }
 
@@ -38,60 +39,60 @@ class Graph {
         for (let key of keys) {
             if (!this.visited.has(key)) {
                 this.nComponent++;
-                this.dfs(key);
+                this.dfs(key, -1);
             }
         }
 
-        return this.nComponent;
+        console.log(`Number of component: ${this.nComponent}`);
     }
 
-    // Graph traversal function using dfs, prints when finding a cycle
-    dfs(start, parent) {
-        const neighbors = this.adjList.get(start);
+    // Graph traversal function using dfs, prints when finding a back edge
+    dfs(vertex, parent) {
+        const children = this.adjList.get(vertex);
 
-        console.log(start);
-        this.visited.add(start);
+        console.log(vertex);
 
-        for (let neighbor of neighbors) {
-            if (!this.visited.has(neighbor))
-                this.dfs(neighbor, start);
-            else if (parent != neighbor && parent !== undefined) {
-                console.log('There is a cycle ! :D');
-            }
+        this.visited.add(vertex);
+
+        for (let child of children) {
+            if (child == parent)
+                continue;
+            if (this.visited.has(child))                                    // {vertex, child} is a back edge
+                console.log(`{${vertex}, ${child}} is a back edge`);
+            else                                                            // {vertex, child} is a forward edge
+                this.dfs(child, vertex);
         }
     }
 }
 
 const graph = new Graph();
-const vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
-const edges = [
-    ['E', 'A'],
-    ['A', 'B'],
-    ['B', 'C'],
-    ['C', 'D'],
-    ['B', 'F'],
-    ['F', 'G'],
-    ['G', 'C'],
-    ['I', 'J'],
-    ['J', 'N'],
-    ['H', 'L'],
-    ['L', 'P'],
-    ['P', 'O'],
-    ['O', 'K'],
-    ['K', 'L']
-];
+const vertices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-// Adding vertices
+// Adding vertices (simulating a game board of a 3v3 size)
 for (let i = 0; i < vertices.length; i++) {
     graph.addVertex(vertices[i]);
 }
 
-// Adding edges
-edges.forEach(edge => graph.addEdge(...edge));
+// Adding edges (simulating a player clicking and creating edges by linking vertices)
+graph.addEdge(0, 4);        // TURN1
+graph.addEdge(4, 5);        // TURN2
+graph.addEdge(1, 2);        // TURN3
+graph.addEdge(2, 6);        // TURN4
+graph.addEdge(2, 3);        // TURN5
+graph.addEdge(6, 7);        // TURN6
+graph.addEdge(3, 7);        // TURN7
+graph.addEdge(10, 11);      // TURN8
+graph.addEdge(9, 10);       // TURN9
+graph.addEdge(5, 9);        // TURN10
+graph.addEdge(8, 12);       // TURN11
+graph.addEdge(12, 13);      // TURN12
+graph.addEdge(13, 14);      // TURN13
+graph.addEdge(14, 15);      // TURN14
+graph.addEdge(15, 11);      // TURN15
+graph.addEdge(10, 14);      // TURN16
 
 // Printing the graph
 graph.printGraph();
 
-// Doing a Depth First Search
-console.log('\n*** DFS ***');
-console.log(graph.countComponent());
+// Counting number of components
+graph.countComponent();
